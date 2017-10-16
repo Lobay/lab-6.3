1403f46a93b83000c3b20054dd756a78c034c746
Lobay <32384708+Lobay@users.noreply.github.com>
2017-10-16 22:07:38 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(int argc, char* argv[])
+{
+string str1 = argv [1];
+char symbol = argv [2][0];
+char symbol = argv [3][0];
+for (int i=0; i < str1.length(); i++)
+{
+   if ( string[i]==symbol)
+   str1[i] = symbol2;
+}
+cout << str1 << endl;
+}
